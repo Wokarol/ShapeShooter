645c50eb5c04a9ba7a44f750b6ddfa71a9471d07
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2018-12-28 11:43:06 +0100

    Added version number; Added some commands to console;

---
+++ Shape Shooter/Assets/Scripts/TerminalCommands.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using CommandTerminal;
+using UnityEngine.SceneManagement;
+
+namespace Wokarol.TerminalCommands
+{
+	public static class TerminalCommands
+	{
+		[RegisterCommand("Scenes", Help = "Show all loaded scenes", MaxArgCount = 0)]
+		public static void ShowScenes (CommandArg[] args)
+		{
+			string text = "Loaded scenes:";
+			for (int i = 0; i < SceneManager.sceneCount; i++) {
+				var scene = SceneManager.GetSceneAt(i);
+				text += $"\n\t{scene.name}";
+			}
+			Terminal.Log(text);
+		}
+
+        [RegisterCommand("Application_Data", Help = "Shows version, author, project name, ...", MaxArgCount = 0)]
+        public static void Data (CommandArg[] args)
+        {
+            Terminal.Log(
+                $"\tVersion: {Application.version} \n" + 
+                $"\tCompanny Name: {Application.companyName} \n" + 
+                $"\tProduct Name: {Application.productName} \n" +
+                $"\tPlatform: {Application.platform} \n" +
+                $"\tPersistent Data Path: {Application.persistentDataPath} \n" +
+                $"\tUnity Version: {Application.unityVersion} \n");
+        }
+	}
+}
+
---
+++ Shape Shooter/Assets/Scripts/VersionDataTextController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Wokarol
+{
+    public class VersionDataTextController : MonoBehaviour
+    {
+        private const string versionTag = "{V}";
+        [SerializeField] TMP_Text text;
+        [SerializeField] string format = $"v. {versionTag}";
+
+        private void Start()
+        {
+            UpdateText();
+        }
+
+        private void OnValidate()
+        {
+            if (text == null) {
+                text = GetComponent<TMP_Text>();
+            }
+
+            if(text != null) {
+                UpdateText();
+            }
+        }
+
+        private void UpdateText()
+        {
+            text.text = format.Replace(versionTag, Application.version);
+        }
+    }
+}
