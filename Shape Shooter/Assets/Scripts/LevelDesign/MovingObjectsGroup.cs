91f7f664c569714abf6ae5bdbcc731e94d63dd72
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-01-06 21:02:41 +0100

    Added first tutorial scene

---
+++ Shape Shooter/Assets/Scripts/LevelDesign/MovingObjectsGroup.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObjectsGroup : MonoBehaviour
+{
+    [System.Serializable]
+    struct MovingObject
+    {
+        public Transform Target { get => target; set => target = value; }
+        public Vector3 MovementOffset { get => movementOffset; set => movementOffset = value; }
+        public AnimationCurve LerpCurve { get => lerpCurve; set => lerpCurve = value; }
+
+        [SerializeField] private Transform target;
+        [SerializeField] private Vector3 movementOffset;
+        [SerializeField] private AnimationCurve lerpCurve;
+
+        Vector3 startPos;
+        Vector3 endPos;
+
+        public void Recenter() {
+            startPos = Target.localPosition;
+            endPos = startPos += MovementOffset;
+        }
+
+        public void Evalute(float time) {
+            var curvedTime = LerpCurve.Evaluate(time);
+            Target.localPosition = Vector3.Lerp(startPos, endPos, curvedTime);
+        }
+    }
+
+    [SerializeField] MovingObject[] movingObjects = new MovingObject[0];
+    [SerializeField] [Range(0, 1)] float LerpValue = 0;
+
+    private void Start() {
+        foreach (var movingObject in movingObjects) {
+            movingObject.Recenter();
+        }
+    }
+
+    private void Update() {
+        foreach (var movingObject in movingObjects) {
+            movingObject.Evalute(LerpValue);
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        foreach (var movingObject in movingObjects) {
+            if (movingObject.Target == null) return;
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(movingObject.Target.position, transform.TransformDirection(movingObject.MovementOffset));
+            Gizmos.DrawSphere(movingObject.Target.position + transform.TransformDirection(movingObject.MovementOffset), 0.2f);
+        }
+    }
+}
